43b7b86405bc083e5e233de4ed4079d0d5a1bc37
SamSantos1jz <samsantos1jz@gmail.com>
2024-11-11 23:40:51 -0300

    chore: incrementando sistema de login

--- crud/urls.py
+++ crud/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from login import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('estoque/', include('estoque.urls'))
-    
+    path('estoque/', include('estoque.urls')),
+    path('login/', views.login, name="login")
 ]
     
