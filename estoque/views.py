bee2e411c8ecf3baf984152f8017b116ed779e94
SamSantos1jz <samsantos1jz@gmail.com>
2024-11-26 21:29:52 -0300

    Criando e funcionalidade fluxo de login.

--- estoque/views.py
+++ estoque/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Produto
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
+@login_required(login_url="/auth/login/")
 def cadastrar_produto(request):
     if request.method == "GET":
         return render(request, 'cadastrar_produto.html')
@@ -22,11 +23,13 @@ def cadastrar_produto(request):
         
         produto.save()
         return redirect('/estoque/cadastrar_produto')
-
+    
+@login_required(login_url="/auth/login/")
 def listar_produto(request):
     produtos = Produto.objects.filter().all
     return render(request,'listar_produto.html', {'produtos':produtos})
 
+@login_required(login_url="/auth/login/")
 def deletar_produto(request, id):
     produto = Produto.objects.get(id=id)
     produto.delete()
--- login/views.py
+++ login/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
-from django.contrib.auth import login as login_django
+from django.contrib.auth import login 
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -12,7 +12,7 @@ def cadastro_usuario(request):
         username = request.POST.get('username')
         senha = request.POST.get('senha')
         email = request.POST.get('email')
-        user = User.objects.filter(username=username, email=email).first()
+        user = User.objects.filter(username=username).first()
 
         if user:
             return HttpResponse("Usuario ou email já existente")
@@ -21,12 +21,23 @@ def cadastro_usuario(request):
         user.save()
         return HttpResponse("Usuario Cadastrado com sucesso!")
 
+def login_usuario(request):
+    if request.method =="GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        user = authenticate(username=username, password=senha)
+
+        if user: 
+            login(request, user)
+            return redirect('/estoque/listar_produto/')
+        else:
+           return HttpResponse("Username e senha não estão certas")
         
 
+       
+           
 
-
-def login_usuario(request):
-   return render(request, 'login.html')
-            
 
    
